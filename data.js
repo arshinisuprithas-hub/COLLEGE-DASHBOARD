62ec93d9bcc7f6c8ebd49994a6d87f4d8f6e1b9f
arshinisuprithas-hub <arshinisupritha.s@gmail.com>
2026-04-19 12:46:55 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+const subjects = {
+  os: {
+    name: "Operating System",
+    modules: [
+      { notes: "Process & Threads basics", link: "https://youtu.be/26QPDBe-NB8" },
+      { notes: "CPU Scheduling", link: "https://youtu.be/YQ_y7y0m1S0" },
+      { notes: "Deadlocks", link: "https://youtu.be/9K2I0U7l8e4" },
+      { notes: "Memory Management", link: "https://youtu.be/8V2gL4yR1dA" },
+      { notes: "File Systems", link: "#" },
+      { notes: "Disk Scheduling", link: "#" }
+    ]
+  },
+
+  crypto: {
+    name: "Cryptography",
+    modules: [
+      { notes: "Classical Ciphers", link: "#" },
+      { notes: "DES & AES", link: "#" },
+      { notes: "Public Key Crypto", link: "#" },
+      { notes: "Hash Functions", link: "#" },
+      { notes: "Authentication", link: "#" },
+      { notes: "Quantum Crypto", link: "#" }
+    ]
+  },
+
+  ml: {
+    name: "Machine Learning",
+    modules: [
+      { notes: "Introduction ML", link: "#" },
+      { notes: "Data Preprocessing", link: "#" },
+      { notes: "Supervised Learning", link: "#" },
+      { notes: "Unsupervised Learning", link: "#" },
+      { notes: "Frameworks", link: "#" },
+      { notes: "Evaluation", link: "#" }
+    ]
+  }
+};
